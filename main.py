8910468a5f1cae68de889919ac3bc8752503ee44
Daniil <oleinik.d_78@mail.ru>
2024-01-28 16:12:25 +0300

    second step

---
+++ main.py
@@ -0,0 +1,7 @@
+someArray = ["111", "22", "sdffs", "kjkkfj", "fhfhfh"]
+
+newArrayOfStringsTreeOrLess = []
+for el in someArray:
+    if len(el) <= 3:
+        newArrayOfStringsTreeOrLess.append(el)
+
